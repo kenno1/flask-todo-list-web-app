545b54a7fefdedbc1285798d7318260f6a9c24ab
kambe.kenta <kenta.k199405@gmail.com>
2021-02-05 20:51:17 +0900

    added render of create view.

--- app.py
+++ app.py
@@ -18,5 +18,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/')
+def index():
+    return render_template('create.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
